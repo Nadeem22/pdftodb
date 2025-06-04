5cf349278023acc963c8335c5d36f8dc999a9675
Mohammad Nadeem <nadeem2107@gmail.com>
2025-06-04 10:12:44 +0530

    test: verify PDFParsingService in context

--- demo/demo/src/test/java/com/nadeem/pdftodb/DemoApplicationTests.java
+++ demo/demo/src/test/java/com/nadeem/pdftodb/DemoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.nadeem.pdftodb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.nadeem.pdftodb.service.PDFParsingService;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private PDFParsingService pdfParsingService;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(pdfParsingService);
+    }
 
 }
